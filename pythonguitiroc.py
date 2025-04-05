b71be01d6bc2a51d9c35427351ec253abb97795d
Lorenzo Festi <festi.lorenzo2000@gmail.com>
2025-04-05 17:45:16 +0200

    "MAIN GUI"

--- pythonguitiroc.py
+++ pythonguitiroc.py
@@ -15,7 +15,7 @@ import pathlib as path
 from import1 import *
 from import5 import *
 from import6 import *
-
+from nacanumber import *
 
 def plot1(): 
     #impostiamo i dati presi dalla GUI, e creaiamo un linspace
@@ -26,82 +26,11 @@ def plot1():
     corda= 1
     x=np.array(linspace(0, float(corda), num=1000))
     naca=nacaa_var.get()
-    #dividiamo naca cifra per cifra
-    arr_naca=list(naca)
-    naca_subtype = len(arr_naca)
-    #normalizziamo per la corda
-    xn=x/float(corda)
     xb=zeros(shape=(200,1))
     yb=zeros(shape=(200,1))
-    #creaiamo 3 casi: naca 4 cifre simmetrichi, naca 4 cifre non simmetrici, naca 5 cifre
-    if naca_subtype == 4:
-        #impostiamo i valori con cui trovare il profilo
-        m=(int(arr_naca[0]))/100
-        p=int(arr_naca[1])/10
-        t=(10*int(arr_naca[2])+int(arr_naca[3]))/100
-        #troviamo l'equazione della mediana
-        if m==0 and p==0:
-            # troviamo equazione della parte superiore e della parte inferiore del profilo
-            yt=(t/0.2)*((0.2969*xn**(0.5))-(0.126*xn)-(0.3516*xn**2)+(0.2843*xn**3)-(0.1015*xn**4))
-            xu=np.array(xn)
-            xl=np.array(xn)
-            yu=np.array(yt)
-            yl=np.array(-yt)
-            ymediana=0*x
-            xf=np.concatenate((np.flip(xu), xl))
-            yf=np.concatenate((np.flip(yu), yl))
-            
-        else:
-            # troviamo equazione della parte superiore e della parte inferiore del profilo
-            yt=(t/0.2)*((0.2969*xn**(0.5))-(0.126*xn)-(0.3516*xn**2)+(0.2843*xn**3)-(0.1015*xn**4))
-            for i in xn:
-                #troviamo l'equazione della mediana
-                if i<p:
-                    ymediana=(m/(p**2))*(2*p*xn-xn**2)
-                elif i>p:
-                    ymediana=(m/(1-p)**2)*((1-2*p)+2*p*xn-xn**2)
-
-            # troviamo equazione della parte superiore e della parte inferiore del profilo
-            teth=np.gradient(ymediana,xn)
-            xu=np.array(xn-yt*sin(teth))
-            xl=np.array(xn+yt*sin(teth))
-            yu=np.array(ymediana+yt*cos(teth))
-            yl=np.array(ymediana-yt*cos(teth))
-            xf=np.concatenate((np.flip(xu), xl))
-            yf=np.concatenate((np.flip(yu), yl))
-    elif naca_subtype == 5:
-        cl=1.5*int(arr_naca[0])
-        if int(arr_naca[1])<=0:
-            return None
-        if int(arr_naca[1])>6:
-            return None
-        
-        p=0.5*(10*int(arr_naca[1])+int(arr_naca[2]))
-        t=10*int(arr_naca[3])+int(arr_naca[4])
-        m_arr=np.array([0.058,0.126,0.2025,0.29,0.391])
-        k_one_arr=np.array([361.4,51.64,15.957,6.643,3.23])
-        arr_naca_int=int(arr_naca[1])
-        m=float(m_arr[arr_naca_int-1])
-        k_one=float(k_one_arr[arr_naca_int-1])
-        yt=(t/0.2)*((0.2969*xn**(0.5))-(0.126*xn)-(0.3516*xn**2)+(0.2843*xn**3)-(0.1015*xn**4))
-
-        for i in xn:
-            if i<p:
-                ymediana=(k_one/6)*(xn**3-3*m*xn**2+xn*(3-m)*m**2)
-            elif i>p:
-                ymediana=(k_one*(m**3)/6)*(1-xn) 
-        teth=np.gradient(ymediana,xn)
-        xu=np.array(xn-yt*sin(teth))
-        xl=np.array(xn+yt*sin(teth))
-        yu=np.array(ymediana+yt*cos(teth))
-        yl=np.array(ymediana-yt*cos(teth))
-        xf=np.concatenate((np.flip(xu), xl))
-        yf=np.concatenate((np.flip(yu), yl))
-    else:
-        return None  # Restituisce None se il numero non ha 4 o 5 arr_naca
-
-
+    nacanum(naca,x)
     #  IMPORTA QUI DENTRO LA FUNZIONE PER CREARE PANNELLI
+
 
     # definiamo spazio del plot
 
@@ -118,15 +47,14 @@ def plot1():
     plot1.set_ylim(-max(xu)*1.2,max(xu)*1.2)
     plot1.plot(xu,yu,'k-',xl,yl,'k-',x,ymediana,'r-',xb,yb,'2') 
 
-
-  
-def calculate(*args):
-    try:
-        value = float(nacaa_var.get())
-        AoA_value.set(float(value*0.1))
-    except ValueError:
-        pass
-
+#
+# def calculate(*args):
+#     try:
+#         value = float(nacaa_var.get())
+#         AoA_value.set(float(value*0.1))
+#     except ValueError:
+#         pass
+#
 # the main Tkinter window 
 window = Tk() 
 window.columnconfigure(3)
@@ -144,12 +72,12 @@ plot_button = Button(master = window,
 					width = 20, 
 					text = "Jukowsky") 
 plot_button2 = Button(master = window, 
-					command = plot1, 
+					command = plot2, 
 					height = 10, 
 					width = 20, 
 					text = "Profili Sottili") 
 plot_button3 = Button(master = window, 
-					command = plot1, 
+					command = plot3, 
 					height = 10, 
 					width = 20, 
 					text = "Metodo a Pannelli") 
